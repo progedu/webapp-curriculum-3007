daca9b38c8e234128aa5e872cacb2babf196c1ae
Yosuke Ogata <ogata0227@outlook.com>
2019-12-10 12:44:10 +0900

    練習問題

--- src/main/scala/CountDownLatch.scala
+++ src/main/scala/CountDownLatch.scala
@@ -1,3 +1,28 @@
+import java.util.concurrent.TimeoutException
+import java.util.concurrent.atomic.AtomicInteger
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Await, Future, Promise}
+import scala.util.{Failure, Random, Success}
+
 object CountDownLatchSample extends App {
-  ???
+
+  private[this] val fs: Seq[Future[Int]] = {
+    for (i <- 1 to 8) yield Future {
+      val waitMilliSec = new Random().nextInt(1000)
+      Thread.sleep(waitMilliSec)
+      waitMilliSec
+    }
+  }
+
+  private[this] var counter = new AtomicInteger(0)
+
+  for (f <- fs;v <- f) {
+    if (counter.incrementAndGet == 3) {
+      for (f <- fs if f.isCompleted; v <- f) println(s"${v} msec")
+    }
+  }
+
+  while(counter.get <= 3) Thread.sleep(100)
+
 }
--- src/main/scala/PromiseStdIn.scala
+++ src/main/scala/PromiseStdIn.scala
@@ -1,6 +1,6 @@
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
+import scala.concurrent.{Await, Future, Promise}
 import scala.util.{Failure, Success}
 
 object PromiseStdIn extends App {
@@ -9,11 +9,20 @@ object PromiseStdIn extends App {
     lineInputProcessor(io.StdIn.readLine().toInt)
   }
 
-  val future: Future[Int] = ???
+  val future: Future[Int] = {
+    val promise: Promise[Int] = Promise[Int]
+    applyFromStdIn(i => promise.success(i * 7))
+    promise.future
+  }
 
-  future onComplete {
+  println(future.isCompleted)
+
+  future.onComplete {
     case Success(value) => println(value)
     case Failure(throwable) => throwable.printStackTrace()
   }
+
   Await.result(future, Duration.Inf)
+  println(future.isCompleted)
+
 }
